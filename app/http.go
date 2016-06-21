ae737b1551c6d33d83a3781eaa3bd7ecfb4f5946
Tim Birkett <tim.j.birkett@gmail.com>
2016-06-21 01:03:12 +0100

    get port from environment, add simple logging

--- app/http.go
+++ app/http.go
@@ -4,14 +4,60 @@ import (
   "fmt"
   "net/http"
   "os"
+  "io"
+  "io/ioutil"
+  "log"
 )
+
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+func Init(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    Trace = log.New(traceHandle,
+        "TRACE: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Info = log.New(infoHandle,
+        "INFO: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Warning = log.New(warningHandle,
+        "WARNING: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Error = log.New(errorHandle,
+        "ERROR: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
   h, _ := os.Hostname()
   fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+  Info.Println("Request served by:", h)
 }
 
 func main() {
+  // Logging is good. Sending to stdout / stderr allows both Docker and
+  // supervisord to deal with the logs.
+  Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+
+  // Make port configurable. This may change depending on where / how it is
+  // deployed
+  port := os.Getenv("PORT")
+  if port == "" {
+      port = "8484"
+  }
+  Warning.Println("Listening on port:", port)
+
   http.HandleFunc("/", handler)
-  http.ListenAndServe(":8484", nil)
+  http.ListenAndServe(":" + port , nil)
 }
