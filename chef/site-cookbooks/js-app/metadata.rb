d2a44177d37dbad8d3b8405522b6530eb67b4fed
Tim Birkett <tim.j.birkett@gmail.com>
2016-06-22 12:56:37 +0100

    Add repo, install http-app package, ensure http-app runs unpriveleged

--- chef/site-cookbooks/js-app/metadata.rb
+++ chef/site-cookbooks/js-app/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures js-app'
 long_description 'Installs/Configures js-app'
 version          '0.1.0'
+
+depends 'yum'
--- chef/site-cookbooks/js-app/recipes/default.rb
+++ chef/site-cookbooks/js-app/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# add the local repo (jenkins server)
+yum_repository 'local-app' do
+  description 'Local app repositpory'
+  baseurl 'http://builds/'
+  enabled true
+  gpgcheck false
+  action :create
+end
+
+package 'http-app' do
+  action :install
+end
